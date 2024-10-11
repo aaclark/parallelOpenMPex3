430b9d455fba8ba218b5bbf0e4cfa64b1547c011
Alain Clark <aac.clark@gmail.com>
2024-10-11 19:19:39 +0200

    renaming and comments for clarity

--- src/matrix_vec_shared_t.cpp
+++ src/matrix_vec_shared_t.cpp
@@ -1,30 +1,33 @@
 #include "matrix_vec_shared_t.hpp"
 
-/**
- * Loop Analysis:
- * for i = lower, upper, stride; { a(i) }
- * --> for i’ = 0, (upper – lower + stride)/stride – 1, 1; {a(i’*stride + lower)}
- * let i' = _L1 ; j' = _L2 ; etc.
- * auto _UPPER_NORM = [](int l, int u, int s)->int{return (u - l + s)/s - 1;};
- * auto _INDEX_NORM = [](int idx, int l, int s)->int{return idx*s+l;};
- */
-auto _norm_N = [](int l, int s, int u)->int{return (u - l + s)/s - 1;};
-auto _norm_fn_idx = [](int l, int s, int u){return [l,s](int idx){return idx*s+l;};};
-//auto INDEX_NORM = [](int l, int s, int i){return i*s+l;};
-
 template <typename T>
 bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
-    int N = A.size();
-    int M = b.size();
-    if (M != N)
+    int A_M = A.size();
+    int b_N = b.size();
+    if (b_N != A_M)
         return false;
     int row, col;
-    x.resize(N);
-//#pragma omp parallel default(none) private(M,N,row,col) shared(A,x,b) num_threads(8)
+    x.resize(A_M); // st. x_N = b_N
+//#pragma omp parallel default(none) private(b_N,A_M,row,col) shared(A,x,b) num_threads(8)
     {
+
+        /**
+         * Loop Analysis:
+         * In general it is always possible to normalize a loop specification from:
+         *      for (i = begin, (i<) end, (i+=) stride) { a(i) }
+         * To:
+         *      for (i’ = 0, (end – begin + stride)/stride – 1, 1) {a(i’*stride + begin)}
+         *
+         * auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
+         * auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
+        */
+        auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
+        auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
+        //auto INDEX_NORM = [](int l, int s, int i){return i*s+l;};
+        
 //#pragma omp for schedule(static)
         // Non-loop-carried
-        for (row = 0; row < N; row++) {
+        for (row = 0; row < A_M; row++) {
             x(row) = b(row);
         }
         /**
@@ -37,23 +40,23 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
          */
 
 
-        // fn. such that j(_1) = j
-        auto j = _norm_fn_idx(0,-1,N);
-        int _1;
-        int _1N = _norm_N(0,-1,N);
-        for (_1 = 0; _1 < _1N; _1++) {
-            x(j(_1)) /= A(j(_1), j(_1));
+        // fn. such that j(L1) = j
+        auto j = _norm_fn_idx(A_M-1, -1, -1);
+        int L1;
+        int L1N = _norm_N(A_M-1, -1, -1);
+        for (L1 = 0; L1 < L1N; L1++) {
+            x(j(L1)) /= A(j(L1), j(L1));
 
-            // fn. such that i(_2) = i
-            auto i = _norm_fn_idx(0,1,j(_1));
-            int _2;
-            int _2N = _norm_N(0,1,j(_1));
-            for (_2 = 0; _2 < _2N; _2++) {
-                x(i(_2)) -= A(i(_2), j(_1)) * x(j(_1));
+            // fn. such that i(L2) = i
+            auto i = _norm_fn_idx(0, 1, j(L1));
+            int L2;
+            int L2N = _norm_N(0, 1, j(L1));
+            for (L2 = 0; L2 < L2N; L2++) {
+                x(i(L2)) -= A(i(L2), j(L1)) * x(j(L1));
             }
         }
 
-//        for (j = N - 1; j >= 0; j--) {
+//        for (j = A_M - 1; j >= 0; j--) {
 //            x(j) /= A(j, j);
 //            for (i = 0; i < j; i++) {
 //                x(i) -= A(i, j) * x(j);
