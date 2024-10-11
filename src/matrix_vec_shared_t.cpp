f56a95d17e16be248571a85280c535d3f364cd24
Alain Clark <aac.clark@gmail.com>
2024-10-11 21:50:19 +0200

    mostly renaming

--- src/matrix_vec_shared_t.cpp
+++ src/matrix_vec_shared_t.cpp
@@ -25,8 +25,8 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
          * auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
          * auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
         */
-        auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
         auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
+        auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
         //auto INDEX_NORM = [](int l, int s, int i){return i*s+l;};
         
 //#pragma omp for schedule(static)
@@ -35,7 +35,7 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
             x(row) = b(row);
         }
 
-        // fn. such that j(L1) = j
+        // fn. such that j(L1) = j = col
         auto j = _norm_fn_idx(A_M-1, -1, -1);
         int L1;
         int L1N = _norm_N(A_M-1, -1, -1);
@@ -50,7 +50,7 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
             // fn. such that i(L2) = i
             auto i = _norm_fn_idx(0, 1, j(L1));
             int L2;
-            int L2N = _norm_N(0, 1, j(L1)); // L2N = {0 .. L1 -1}
+            int L2N = _norm_N(0, 1, j(L1)); // L2N = {0 .. L1-1}
             for (L2 = 0; L2 < L2N; L2++) {
                 T val_x_L2 = x(i(L2)); // READ
                 T val_A_L2_L1 = (A(i(L2), j(L1))); // READ
@@ -61,12 +61,26 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
             }
         }
 
-//        for (j = A_M - 1; j >= 0; j--) { // j = {M-1, M-2, M-3,..., 0} ==> L1 = {0, 1, 2, ... M-1}
-//            x(j) /= A(j, j);
-//            for (i = 0; i < j; i++) { // i = {0, 1, ..., j-1} ==> L2 = {0, 1, ..., L1-1}
-//                x(i) -= A(i, j) * x(j);
-//            }
-//        }
+        /**
+        for (row = 0; row < n; row++)
+            x[row] = b[row];
+        for (col = n-1; col >= 0; col--) {
+            x[col] /= A[col][col];
+            for (row = 0; row < col; row++)
+                x[row] -= A[row][col] * x[col];
+        }
+        */
+
+        /**
+        for (row = 0; row < n; row++)
+            x[row] = b[row];
+        for (col = n-1; col >= 0; col--) {
+            x[col] /= A[col][col];
+            for (row = 0; row < col; row++)
+                x[row] -= A[row][col] * x[col];
+        }
+        */
+
     }
     return true;
 }
@@ -74,17 +88,76 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
 
 template <typename T>
 bool solve_r(matrix<T>& A,vec<T>& x, vec<T>& b) {
-    int N = A.size();
-    int M = b.size();
-    if (M != N)
+    int A_M = A.size(); // A_M = width(M)
+    int b_N = b.size(); // b_N = |b|
+    if (b_N != A_M)
         return false;
     int row, col;
-    x.resize(N);
-    for (row = N-1; row >= 0; row--) {
-        x(row) = b(row);
-        for (col = row+1; col < N; col++)
-            x(row) -= A(row, col) * x(col);
-        x(row) /= A(row, row);
+    x.resize(A_M); // such that |x| = |b| and |x| = width(M)
+    {
+        /**
+         * Loop Analysis:
+         * In general it is always possible to normalize a loop specification from:
+         *      for (i = begin, (i<) end, (i+=) stride) { a(i) }
+         * To:
+         *      for (i’ = 0, (end – begin + stride)/stride – 1, 1) {a(i’*stride + begin)}
+         * Such that for each loop depth we have:
+         *      for (L1 : 0 .. M)
+         *          for (L2 : 0 .. N)
+         *              ...
+         *
+         * auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
+         * auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
+        */
+        auto _norm_fn_idx = [](int begin, int stride, int end){return [begin,stride](int idx){return idx * stride + begin;};};
+        auto _norm_N = [](int begin, int stride, int end)->int{return (end - begin + stride) / stride - 1;};
+
+        // fn. such that j(L1) = j = row
+        auto j = _norm_fn_idx(A_M-1, -1, -1);
+        int L1;
+        int L1N = _norm_N(A_M-1, -1, -1);
+        // for (L1 : 0 .. L1N)
+
+        for (L1 = 0; L1 < L1N; L1++) {
+            x(j(L1)) = b(j(L1));
+
+            // fn. such that i(L2) = i
+            auto i = _norm_fn_idx(j(L1)+1, 1, A_M);
+            int L2;
+            int L2N = _norm_N(0, 1, j(L1));
+
+            for (L2 = 0; L2 < L2N; L2++)
+                x(j) -= A(j, i) * x(i);
+            x(j) /= A(j, j);
+        }
+
+        /**
+        for (j = n-1; j >= 0; j--) {
+            x[j] = b[j];
+            for (i = j+1; i < n; i++)
+                x[j] -= A[j][i] * x[i];
+            x[j] /= A[j][j];
+        }
+         */
+
+        /**
+        for (j = n-1; j >= 0; j--) {
+            x[j] = b[j];
+            for (i = j+1; i < n; i++)
+                x[j] -= A[j][i] * x[i];
+            x[j] /= A[j][j];
+        }
+         */
+
+        /**
+        for (row = n-1; row >= 0; row--) {
+            x[row] = b[row];
+            for (col = row+1; col < n; col++)
+                x[row] -= A[row][col] * x[col];
+            x[row] /= A[row][row];
+        }
+         */
+
     }
     return true;
 }
