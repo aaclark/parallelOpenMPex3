173128082c9baedfc25446b1d1552f3130d0b38a
Alain Clark <aac.clark@gmail.com>
2024-10-11 17:34:36 +0200

    loop analysis pt1

--- src/matrix_vec_shared_t.cpp
+++ src/matrix_vec_shared_t.cpp
@@ -11,9 +11,21 @@ bool solve_c(matrix<T>& A, vec<T>& x, vec<T>& b) {
 //#pragma omp parallel default(none) private(M,N,row,col) shared(A,x,b) num_threads(8)
     {
 //#pragma omp for schedule(static)
+        // Non-loop-carried
         for (row = 0; row < N; row++) {
             x(row) = b(row);
         }
+        /**
+         * Loop Analysis:
+         * for i = lower, upper, stride; { a(i) }
+         * --> for i’ = 0, (upper – lower + stride)/stride – 1, 1; {a(i’*stride + lower)}
+         */
+        int _L1 = 0;
+        int _L1_N = ((N - 1)-(0)+(-1))/(-1)-1;
+        auto _L1_i = [_L1]()->int{return ((-1)*_L1)+(0);};
+        int _L2 = 0;
+        int _L2_N = ((N - 1)-(0)+(-1))/(-1)-1;
+        auto _L2_i = [_L1]()->int{return ((-1)*_L1)+(0);};
         for (col = N - 1; col >= 0; col--) {
             x(col) /= A(col, col);
             for (row = 0; row < col; row++) {
