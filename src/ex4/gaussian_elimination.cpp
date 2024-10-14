391779b9e4044757f5ebea9d123fd6945f1a7043
Alain Clark <aac.clark@gmail.com>
2024-10-14 03:48:54 +0200

    overhaul main for rigorous testing

--- src/ex4/gaussian_elimination.cpp
+++ src/ex4/gaussian_elimination.cpp
@@ -57,53 +57,45 @@ int main(int argc, char* argv[]) {
     matrix<double_t> A; // Declare
     vec<double_t> b, x, y;    // Declare
     //uniform_double(engine)
-    A.resize(3);
-    //.upper(2.0);     // Modify: set upper triangle to V
-    b.resize(A.size());
+    A.resize(size_N)
+    .upper(9.0).diagonal(7.0);     // Modify: set upper triangle to V
+    b.resize(A.size(),3.0);
 
-    A(0,0) = 2.0;   A(0,1) =-3.0;
-                            A(1,1) = 1.0;   A(1,2) = 1.0;
-                                                    A(2,2) = -5.0;
+//    A(0,0) = 2.0;   A(0,1) =-3.0;
+//                            A(1,1) = 1.0;   A(1,2) = 1.0;
+//                                                    A(2,2) = -5.0;
+//
+//    b(0) = 3.0;
+//    b(1) = 1.0;
+//    b(2) = 0.0;
 
-    b(0) = 3.0;
-    b(1) = 1.0;
-    b(2) = 0.0;
+//    std::cout<<"A="<<std::endl;A.show(); std::cout << std::endl << std::endl;
+//    std::cout<<"b="<<std::endl;b.show(); std::cout << std::endl << std::endl;
 
-    std::cout<<"A="<<std::endl;A.show(); std::cout << std::endl << std::endl;
-    std::cout<<"b="<<std::endl;b.show(); std::cout << std::endl << std::endl;
 
-    // Start the clock!
-    auto t1 = high_resolution_clock::now();
-
-    /**
-     * DO TIMED STUFF HERE
-     */
-
-    // Ax = b
-    solve_c(A, x, b);
-    //x.show(); std::cout << std::endl << std::endl;
-
-    // Stop the clock!
-    auto t2 = high_resolution_clock::now();
-    duration<double, std::milli> ms_double_c = t2 - t1;
-
-    /**
-     * DO TIMED STUFF HERE
-     */
-
-    // Ax = b
+    std::cout << "Start R." << std::endl;
+    auto r_t1 = high_resolution_clock::now();
     solve_r(A, y, b);
+    auto r_t2 = high_resolution_clock::now();
 
-    // Stop the clock!
-    auto t3 = high_resolution_clock::now();
-    duration<double, std::milli> ms_double_r = t3 - t2;
+
+    std::cout << "Start C." << std::endl;
+    auto c_t1 = high_resolution_clock::now();
+    solve_c(A, x, b);
+    auto c_t2 = high_resolution_clock::now();
+
+    duration<double, std::milli> ms_double_c = c_t2 - c_t1;
+    duration<double, std::milli> ms_double_r = r_t2 - r_t1;
+
+    std::cout << "x = "; x.show(); std::cout << std::endl << std::endl;
+    std::cout << "y = "; y.show(); std::cout << std::endl << std::endl;
 
     // Print results and net runtime of running <STUFF>
     std::cout << std::left<< std::setfill(' ')
-    << "N="         << std::setw(8) << std::setprecision(4) << size_N
-    //<< "Threads="   << std::setw(8) << std::setprecision(4) << c.thr
-    << "solve_c="        << std::setw(8) << std::setprecision(4) << ms_double_c.count() << "ms "
-    << "solve_r="        << std::setw(8) << std::setprecision(4) << ms_double_r.count() << "ms "
+        << " N= " << std::setw(12) << std::setprecision(4) << size_N
+        //<< "Threads="   << std::setw(8) << std::setprecision(4) << c.thr
+        << " solve_c= " << std::setw(12) << std::setprecision(4) << (ms_double_c).count() << " ms "
+        << " solve_r= " << std::setw(12) << std::setprecision(4) << (ms_double_r).count() << " ms "
     << std::endl;
 
     return EXIT_SUCCESS;
